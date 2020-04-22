6573fcfa4e06592a6de0106c1a8c445eda37e23a
randiveomkar95 <randiveomkar95@gmail.com>
2020-04-22 22:24:05 +0530

    Change STATIC_URL Path

--- textutils/settings.py
+++ textutils/settings.py
@@ -123,7 +123,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
 
-STATIC_URL = 'static'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, STATIC_URL)
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
